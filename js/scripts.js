ff005e6d601d279db367d0f596bef9c5258f8f49
PaoloIncollu <paoloinco@gmail.com>
2024-08-30 18:40:40 +0200

    caricamento esercizio fino a bonus 1

--- js/scripts.js
+++ js/scripts.js
@@ -1,33 +1,57 @@
 
 const { createApp } = Vue;
 createApp({
-data() {
-  return {
-    list:[
-      {
-        text:'Acqua',
-        done: true
+  data() {
+    return {
+      list:[
+        {
+          text:'Acqua',
+          done: true
+        },
+        {
+          text:'Latte',
+          done: false
+        },
+        {
+          text:'Pane',
+          done: true
+        },
+        {
+          text:'Carne',
+          done: false
+        },
+        {
+          text:'Pesce',
+          done: true
+        },
+        {
+          text:'Frutta',
+          done: false
+        },
+      ],
+      
+        
+      newProduct:'',
+
+    }
+  },
+    methods:{
+      //funzione per rimuovere il prodotto
+      removeProduct(i){
+
+        this.list.splice(i,1);
       },
-      {
-        text:'Latte',
-        done: false
-      },
-      {
-        text:'Pane',
-        done: true
-      },
-      {
-        text:'Carne',
-        done: false
-      },
-      {
-        text:'Pesce',
-        done: true
-      },
-      {
-        text:'Frutta',
-        done: false
-      },
-    ]
-  }
-}}).mount('#app');
+
+      //funzione per aggiungere un prodotto alla lista
+      addProduct(){
+
+        const cleanTodo = this.newProduct.trim();
+        if(cleanTodo != ''){
+
+          this.list.push({text:cleanTodo, done:false});
+          console.log(this.list);
+          this.newProduct = '';
+
+        }
+      }}
+}).mount('#app')
